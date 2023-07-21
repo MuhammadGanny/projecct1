51b19c4feec0f46b2a147d53b816b4fb76ff9787
muhammadganny <mohammadganny@gmail.com>
2023-07-21 12:49:57 +0500

    adding details and simplify design

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
         <div className="picture-con">
           <img className="picture" src= {logo} />
         </div>
+        <div className="details">
+          <h3>Muhammad Bin Abdul Ganny</h3>
+          <h4>Full Stack Developer</h4>
+          <h5>www.website.comS</h5>
+          <h2>About</h2>
+          <p>I am a full stack developer starting my journy fromm systems </p>
+          <h2>Intrest</h2>
+          <p>I am a car enthusiast and love doing gym  </p>
+        </div>
+        
       </div>
       
     </div>
