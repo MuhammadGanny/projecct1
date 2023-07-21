96290e295eaf0984d6536148ce7f39e7cfd9bc20
muhammadganny <mohammadganny@gmail.com>
2023-07-21 12:10:18 +0500

    added the picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="Whole-Conatainer">
       <div className="card">
-        <h1>hellow world </h1>
+        <div className="picture-con">
+          <img className="picture" src= {logo} />
+        </div>
       </div>
       
     </div>
